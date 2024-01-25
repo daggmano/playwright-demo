1c77074544c75bc6a6984f7302b3970e0c69fac9
Darren Oster <darren.oster@gmail.com>
2024-01-25 11:59:34 +1030

    Renaming

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
-import App from './App';
+import App from './Appb';
 import { Todo } from './todo/todo'; 
 import reportWebVitals from './reportWebVitals';
 
